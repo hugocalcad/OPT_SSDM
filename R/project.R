e69f30714323a1f6bfd3672bb318349f7aff945b
hugocalcad <hugocalcad@gmail.com>
2019-05-05 15:22:50 -0400

    Split Raster add spaDEs.Tools y trabajar el split en project.r

--- R/project.R
+++ R/project.R
@@ -28,7 +28,7 @@ setMethod("project", "Algorithm.SDM", function(obj, Env, ...) {
   ##reclasificar
     listado_completo <- list.files(paste0(path, "/.rasters_2") , pattern = paste("*tile",i,".gri$", sep = ''), recursive = TRUE, full.names = TRUE )
     Env_temp <- raster::stack(listado_completo)
-    proj_2[i] = suppressWarnings(raster::predict(Env_temp, model, fun = function(model,x) {
+    proj_3 = suppressWarnings(raster::predict(Env_temp, model, fun = function(model,x) {
       x = as.data.frame(x)
       for (j in seq_len(length(Env_temp@layers))) {
         if (Env_temp[[j]]@data@isfactor) {
@@ -39,15 +39,15 @@ setMethod("project", "Algorithm.SDM", function(obj, Env, ...) {
       }
       return(predict(model, x))
     }))
-    
-    proj_2[i] = reclassify(proj_2[i], c(-Inf, 0, 0))
+    # Rescaling projection
+    proj_2[i] = reclassify(proj_3, c(-Inf, 0, 0))
   }  
   ##Mosaic -> recomponiendo los rasters
   proj <- mergeRaster(proj_2)
   unlink(file.path(path, ".rasters_2"), recursive=TRUE)
   ##
   # Rescaling projection
-  # proj = reclassify(proj, c(-Inf, 0, 0))
+  #proj = reclassify(proj, c(-Inf, 0, 0))
   if(all(obj@data$Presence %in% c(0,1))) # MEMs should not be rescaled
     proj = proj / proj@data@max
   names(proj) = "Projection"
@@ -76,24 +76,27 @@ setMethod("project", "MAXENT.SDM", function(obj, Env, ...) {
   proj_2 <- list()
   for(i in seq_len(n*n)){
     ##reclasificar
+    listado_completo <- list.files(paste0(path, "/.rasters_2") , pattern = paste("*tile",i,".gri$", sep = ''), recursive = TRUE, full.names = TRUE )
+    Env_temp <- raster::stack(listado_completo)
     
-    proj_2 = raster::predict(Env, model, fun = function(model, x) {
+    proj_3 = raster::predict(Env_temp, model, fun = function(model, x) {
       x = as.data.frame(x)
-      for (i in seq_len(length(Env@layers))) {
-        if (Env[[i]]@data@isfactor) {
-          x[, i] = as.factor(x[, i])
-          x[, i] = droplevels(x[, i])
-          levels(x[, i]) = Env[[i]]@data@attributes[[1]]$ID
+      for (j in seq_len(length(Env@layers))) {
+        if (Env[[j]]@data@isfactor) {
+          x[, j] = as.factor(x[, j])
+          x[, j] = droplevels(x[, j])
+          levels(x[, j]) = Env_temp[[j]]@data@attributes[[1]]$ID
         }
       }
       return(predict(model, x))
     })
     # Rescaling projection
-    proj_2 = reclassify(proj_2, c(-Inf, 0, 0))
+    proj_2[i] = reclassify(proj_3, c(-Inf, 0, 0))
   }
   ##Mosaic -> recomponiendo los rasters
   proj <- mergeRaster(proj_2)
   unlink(file.path(path, ".rasters_2"), recursive=TRUE)
+  #proj = reclassify(proj, c(-Inf, 0, 0))
   ##
   if(!all(obj@data$Presence %in% c(0,1))) # MEMs should not be rescaled
     proj = proj / proj@data@max
